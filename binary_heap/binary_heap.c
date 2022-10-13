022b07970c153bb90f07e5acabd0256b0f845a3e
mroik <mroik@delayed.space>
2022-10-13 23:44:33 +0200

    Add "Binary heap" implementation

---
+++ binary_heap/binary_heap.c
@@ -0,0 +1,43 @@
+void sift_down(int* data, int index, int size)
+{
+	int i = index;
+	int l, r;
+	int greater = i;
+
+	while(i < size)
+	{
+		l = i * 2 + 1;
+		r = i * 2 + 1;
+		if(l >= size)
+			break;
+
+		if(data[i] < data[l])
+			greater = l;
+
+		if(r < size && data[greater] < data[r])
+			greater = r;
+
+		if(greater == i)
+			break;
+
+		int temp = data[greater];
+		data[greater] = data[i];
+		data[i] = temp;
+		i = greater;
+	}
+}
+
+int* make_heap(int* from, int size)
+{
+	for(int x = size - 1; x >= 0; x--)
+		sift_down(from, x, size);
+}
+
+int pop(int* data, int size)
+{
+	int ris = data[0];
+	data[0] = data[size - 1];
+	size--;
+	sift_down(data, 0, size);
+	return ris;
+}
---
+++ binary_heap/binary_heap.h
@@ -0,0 +1,3 @@
+void sift_down(int* data, int index, int size);
+int* make_heap(int* from, int size);
+int pop(int* data, int size);
