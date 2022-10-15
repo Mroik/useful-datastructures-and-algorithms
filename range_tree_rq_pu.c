262a6ad42c29a32dc0603ac815af82179dd6b06f
mroik <mroik@delayed.space>
2022-10-15 23:51:30 +0200

    Add clarifications on the "Range tree"

--- range_tree_rq_pu.c
+++ range_tree_rq_pu.c
@@ -22,6 +22,10 @@ node* make_node(int value, int start, int end)
 	return new_node;
 }
 
+// This tree in particular is used to query the sum of
+// the items of the specified range. It is not the only
+// way to use it. The implementation could be changed so
+// that each nodes stores the max value of it's range.
 node* build_father(node* left, node* right)
 {
 	int v_left, v_right, start, end;
@@ -30,6 +34,8 @@ node* build_father(node* left, node* right)
 	start = left == NULL ? right->start : left->start;
 	end = right == NULL ? left->end : right->end;
 
+	// This line is the one that defines the usecase of the
+	// range tree
 	node* father = make_node(v_left + v_right, start, end);
 	father->left = left;
 	father->right = right;
