bc2cc92cf48de330b31ae802f67e464cf32eaa5a
Mroik <mroik@delayed.space>
2023-04-26 00:04:10 +0200

    Implement pairing heap the proper rust way

--- src/pairing_heap.rs
+++ src/pairing_heap.rs
@@ -1,85 +1,122 @@
-// TODO Change implementation when https://github.com/rust-lang/rust/issues/83163
-// is fixed (might look like a cheat but is the best way to keep it selfcontained).
-// Learnt about it here: https://github.com/apasel422/pairing-heap/blob/70af61645049fc0d64c02ac32dec672e0002bc6e/src/lib.rs#L21
+use std::{cell::{RefCell, RefMut}, mem};
 
-struct Node<'a> {
-    value: i32,
-    children: Vec<&'a mut Node<'a>>,
+struct Node<T> {
+    value: T,
+    children: RefCell<Vec<Box<Node<T>>>>,
 }
 
-impl<'a> Node<'a> {
-    fn new(value: i32) -> Node<'a> {
-        Node { value, children: Vec::new() }
-    }
-
-    fn top(&self) -> i32 {
-        self.value
-    }
-
-    fn meld(h1: &'a mut Node<'a>, h2: &'a mut Node<'a>) -> &'a mut Node {
-        if h1.value < h2.value {
-            h1.children.push(h2);
-            return h1;
+impl<T: Copy + Ord> Node<T> {
+    fn new(value: T) -> Node<T> {
+        Node {
+            value,
+            children: RefCell::new(vec![]),
         }
-        h2.children.push(h1);
-        return h2;
     }
 
-    fn insert(&'a mut self, other: &'a mut Node<'a>) -> &'a mut Node {
-        return Node::meld(self, other);
+    fn top(&self) -> T {
+        return self.value;
     }
 
-    fn merge_pairs(nodes: &'a mut Vec<&mut Node<'a>>) -> Option<&'a mut Node<'a>> {
-        let n1;
+    fn meld(&mut self, mut h2: Box<Node<T>>) {
+        if self.value < h2.value {
+            self.children.borrow_mut().push(h2);
+            return;
+        }
+        mem::swap(self, &mut h2);
+        self.children.borrow_mut().push(h2);
+    }
+
+    fn push(&mut self, value: T) {
+        let other = Node::new(value);
+        self.meld(Box::new(other));
+    }
+
+    fn merge_pairs(mut nodes: RefMut<Vec<Box<Node<T>>>>) -> Option<Box<Node<T>>> {
+        let mut n1;
         let n2;
-        if nodes.is_empty() {
-            return None;
+        {
+            if nodes.is_empty() {
+                return None;
+            }
+            if nodes.len() == 1 {
+                return Some(nodes.remove(0));
+            }
+            n1 = nodes.remove(0);
+            n2 = nodes.remove(0);
         }
-        if nodes.len() == 1 {
-            return Some(nodes.remove(0));
-        }
-        n1 = nodes.remove(0);
-        n2 = nodes.remove(0);
+
         match Node::merge_pairs(nodes) {
-            None => return Some(Node::meld(n1, n2)),
-            Some(rest) => return Some(Node::meld(Node::meld(n1, n2), rest)),
+            None => {
+                n1.meld(n2);
+                return Some(n1)
+            },
+            Some(rest) => {
+                n1.meld(n2);
+                n1.meld(rest);
+                return Some(n1);
+            },
         }
     }
 
-    fn pop(&'a mut self) -> Option<&mut Node<'a>> {
-        return Node::merge_pairs(&mut self.children);
+    fn pop(&self) -> Option<Box<Node<T>>> {
+        let a = self.children.borrow_mut();
+        return Node::merge_pairs(a);
     }
 }
 
+struct PriorityQueue<T> {
+    root: Option<Box<Node<T>>>,
+}
 
-// With this one I'm keeping the main too.
-// It's hard to keep track of all the lifetimes, this is a somewhat
-// ok example to reference in the future.
-fn main() {
-    let mut resources: Vec<Node> = Vec::new();
-    resources.push(Node::new(10));
-    resources.push(Node::new(3));
-    resources.push(Node::new(8));
-    resources.push(Node::new(7));
-    resources.push(Node::new(1));
-    resources.push(Node::new(6));
-    resources.push(Node::new(2));
-    resources.push(Node::new(9));
-    resources.push(Node::new(4));
-    resources.push(Node::new(5));
-
-    let mut root = resources.remove(0);
-    let mut root_ref = &mut root;
-
-    for x in resources.iter_mut() {
-        root_ref = root_ref.insert(x);
+impl<T: Copy + Ord> PriorityQueue<T> {
+    fn new() -> Self {
+        PriorityQueue {
+            root: None,
+        }
     }
+
+    fn push(&mut self, value: T) {
+        if let Some(rr) = &mut self.root {
+            (*rr).push(value);
+            return;
+        }
+        self.root = Some(Box::new(Node::new(value)));
+    }
+
+    fn top(&self) -> Option<T> {
+        if let Some(root) = &self.root {
+            return Some(root.top());
+        }
+        return None;
+    }
+
+    fn pop(&mut self) -> Result<(), ()> {
+        if let Some(root) = &mut self.root {
+            self.root = root.pop();
+            return Ok(());
+        }
+        return Err(());
+    }
+}
+
+fn main() {
+    let mut queue: PriorityQueue<i32> = PriorityQueue::new();
+    queue.push(10);
+    queue.push(3);
+    queue.push(8);
+    queue.push(7);
+    queue.push(1);
+    queue.push(6);
+    queue.push(2);
+    queue.push(9);
+    queue.push(4);
+    queue.push(5);
 
     for _ in 0..10 {
-        println!("{}", root_ref.top());
-        match root_ref.pop() {
-            None => break,
-            Some(next_ref) => root_ref = next_ref,
+        println!("{}", queue.top().unwrap());
+        match queue.pop() {
+            Ok(_) => (),
+            Err(_) => break,
         }
     }
 }
